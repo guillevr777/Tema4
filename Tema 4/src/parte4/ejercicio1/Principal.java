68dbfed5320429717148cb40d31ec7afa2571f5e
guillevr777 <gs.villanueva@iesnervion.es>
2025-02-06 20:00:53 +0100

    jk

---
+++ Tema 4/src/parte4/ejercicio1/Principal.java
@@ -0,0 +1,49 @@
+package parte4.ejercicio1;
+
+import java.util.Scanner;
+
+import parte3.ejercicio1.Punto;
+
+	public class Principal {
+
+		public static void main(String[] args) {
+		
+			//llamamos al escaner
+			Scanner reader = new Scanner(System.in);
+				
+			//variables para coordenadas x e y
+			int x = 0;
+			int y = 0;
+				
+			//creamos el metodo
+			Punto numero1 = new Punto(5, 0);
+				
+			//metodo de punto
+			Punto numero3 = new Punto(10, 10);
+				
+			//metodo de punto
+			Punto numero2 = new Punto(-3, 7);
+				
+			//mostramos las cooredenadas
+			System.out.println(numero1.getX() +  "," + numero1.getY());
+					
+			//mostramos las cooredenadas
+			System.out.println(numero2.getX() +  "," + numero2.getY());
+				
+			//mostramos las cooredenadas
+			System.out.println(numero3.getX() +  "," + numero3.getY());
+			
+			System.out.println("Que valor quieres darle a X e Y?");
+			x = reader.nextInt();
+			y = reader.nextInt();
+				
+			//asignamos valores a 
+			numero1.setX(x);
+			numero1.setY(y);
+				
+			//mostramos las cooredenadas
+			System.out.println(numero1.getX() +  "," + numero1.getY());
+		
+			System.out.println(numero1.getX() * numero3.getX() + "," + numero2.getY() + numero3.getY());
+	}
+}
---
+++ Tema 4/src/parte4/ejercicio1/Punto.java
@@ -0,0 +1,28 @@
+package parte4.ejercicio1;
+
+	public class Punto {
+		private int x = 0;
+		private int y = 0;
+		
+		/**
+		 * Creamos un constructor con el nombre Punto
+		 * @param x Sirve para guardar las coordenadas X
+		 * @param y Sirve para guardar las coordenadas Y
+		 */
+		public Punto(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		public int getX() {
+			return x;
+		}
+		public int getY() {
+			return y;
+		}
+		public void setX(int x) {
+			this.x = x;
+		}
+		public void setY(int y) {
+			this.y = y;
+		}
+	}
